3ac52ac8d4aa569a813a1fa32dd5c2c235f14b0e
yangfan <1301747127@qq.com>
2022-12-22 15:20:39 +0800

    初始化提交

--- src/preload/main.ts
+++ src/preload/main.ts
@@ -1,9 +1,8 @@
-import { aaa } from "./version/main";
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
-aaa(contextBridge);
-// contextBridge.exposeInMainWorld("versions", {
-//   node: () => process.versions.node,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron,
-// });
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
+});
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ImagesView from "../views/docker/ImagesView.vue";
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
     {
       path: "/workbench",
       name: "workbench",
