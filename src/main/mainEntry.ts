1d14052d31f2c232fbf5f7cebb9cd5a0d4fad808
yangfan <1301747127@qq.com>
2023-02-03 17:06:30 +0800

    初始化提交

--- src/main/mainEntry.ts
+++ src/main/mainEntry.ts
@@ -1,4 +1,9 @@
-import { app, BrowserWindow } from "electron";
+import {
+  app,
+  BrowserWindow,
+  Menu,
+  type MenuItemConstructorOptions,
+} from "electron";
 import path from "path";
 import { handle as ipcMainHandle } from "./ipcMainHandle";
 
@@ -25,6 +30,85 @@ const createWindow = () => {
   mainWindow.loadURL("http://localhost:5173");
   ipcMainHandle(mainWindow);
 };
+
+const template: Array<MenuItemConstructorOptions> = [
+  {
+    label: "Apipost",
+    role: "windowMenu",
+    submenu: [
+      {
+        label: "检查更新",
+        // click() {
+        //   mainWindow.webContents.send("action", "checkUpdate");
+        // },
+        accelerator: "CmdOrCtrl+U",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "退出",
+        role: "quit",
+        accelerator: "CmdOrCtrl+Q",
+      },
+    ],
+  },
+  {
+    label: "编辑",
+    submenu: [
+      {
+        label: "撤销",
+        accelerator: "CmdOrCtrl+Z",
+        role: "undo",
+      },
+      {
+        label: "重做",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        role: "redo",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "复制",
+        accelerator: "CmdOrCtrl+C",
+        role: "copy",
+      },
+      {
+        label: "剪切",
+        accelerator: "CmdOrCtrl+X",
+        role: "cut",
+      },
+      {
+        label: "粘贴",
+        accelerator: "CmdOrCtrl+V",
+        role: "paste",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "全选",
+        accelerator: "CmdOrCtrl+A",
+        role: "selectAll",
+      },
+    ],
+  },
+  {
+    label: "关于",
+    role: "help",
+    submenu: [
+      {
+        label: "关于",
+        // click() {
+        //   mainWindow.webContents.send("windowMenuClick", "about");
+        // },
+      },
+    ],
+  },
+];
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 app.whenReady().then(() => {
   createWindow();
